8836ced9d8a14a11bedbdbbe54b10fb91d15d4b0
Michael <michael.spengler@sap.com>
2022-09-28 00:01:50 +0200

    add potential challenges of the day

--- frontend/cult-playground-challenge-of-the-day/src/stores.js
+++ frontend/cult-playground-challenge-of-the-day/src/stores.js
@@ -12,16 +12,31 @@ export const CultGameProposalStore = writable([
   {
     id: 1,
     rating: 10,
-    text: 'Ensure the following hashtags are trending on Twitter today: #StarlinkIsCULT #RumbleIsCULT #CULTDAO. You might want to do so by retweeting: https://twitter.com/Peer2peerE/status/1574787191489941506?s=20&t=wUyNpuR8Sc7DtcPo7lp37A.',
+    text: 'Ensure #VitalikLovesCULT is trending on Twitter.',
   },
   {
     id: 2,
     rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Follow 20 new people today who might love the CULTDAO to expand our network.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Invite 10 of your friends who might love the CULTDAO to https://discord.com/invite/wearecultdao to expand our network.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'To all who use facebook: Offer each of your facebook friends 1.000 RVLT if they post a supportive statement regarding the CULTDAO.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Ensure #ElonMuskLovesCULT is trending on Twitter.',
   },
   {
     id: 3,
     rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: 'Ensure #SaylorLovesCULT is trending on Twitter.',
   },
 ])
