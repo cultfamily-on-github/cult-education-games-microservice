6b6d21a3e37cc1147f9a12612afb88f7fec44dd3
Michael <michael.spengler@sap.com>
2022-09-27 17:58:33 +0200

    test gh pages based deployment again

--- frontend/cult-education-games-ui/prepare-deployment-via-gh-pages.sh
+++ frontend/cult-education-games-ui/prepare-deployment-via-gh-pages.sh
@@ -3,6 +3,5 @@
 sed -i'' -e 's/href="\//href=".\//g' ./dist/index.html
 
 rm -rf ./../../docs-backup 
-cp -r ./../../docs ./../../docs-backup 
-rm -rf ./../../docs 
+mv ./../../docs ./../../docs-backup 
 cp -r ./dist ./../../docs
