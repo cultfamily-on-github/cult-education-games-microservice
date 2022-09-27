d09ee6c2a465454f78249a87bd96d2fd7f6894fb
Michael <michael.spengler@sap.com>
2022-09-27 22:52:53 +0200

    add state which is ready for first deployment to collect feedback

--- frontend/cult-playground-challenge-of-the-day/src/App.svelte
+++ frontend/cult-playground-challenge-of-the-day/src/App.svelte
@@ -1,14 +1,30 @@
 <script>
   import ShowDetailsButton from "./components/buttons/ShowDetailsButton.svelte";
+  import ShowPhilosophyButton from "./components/buttons/ShowPhilosophyButton.svelte";
+  import GameOfTheDay from "./components/GameOfTheDay.svelte";
+  import Philosophy from "./components/texts/Philosophy.svelte";
+  import HowItWorks from "./components/texts/HowItWorks.svelte";
+  import Levels from "./components/texts/Levels.svelte";
+
   // import FeedbackForm from "./components/feedbackComponents/FeedbackForm.svelte";
   // import FeedbackList from "./components/FeedbackList.svelte";
   // import FeedbackStats from "./components/FeedbackStats.svelte";
-  import GameOfTheDay from "./components/GameOfTheDay.svelte";
-  import HowItWorks from "./components/texts/HowItWorks.svelte";
-  import Levels from "./components/texts/Levels.svelte";
-  import Motivation from "./components/texts/Motivation.svelte";
-  import SupportOrGiveFeedbackRequest from "./components/SupportOrGiveFeedbackRequest.svelte";
-  let showDetailsInParent = true;
+
+  let showDetails = true;
+  let showPhilosophy = true;
+
+  function changeShowDetails(newValue) {
+    showDetails = newValue;
+    if (showDetails) {
+      showPhilosophy = false
+    }
+  }
+  function changeShowPhilosophy(newValue) {
+    showPhilosophy = newValue;
+    if (showPhilosophy) {
+      showDetails = false
+    }
+  }
 </script>
 
 <main class="container">
@@ -17,19 +33,23 @@
 
     <GameOfTheDay />
     <Levels />
-    <ShowDetailsButton onChange={(newValue) => {showDetailsInParent = newValue}}>Show Details</ShowDetailsButton>
 
-    {#if showDetailsInParent}
+    <ShowDetailsButton onChange={(newValue) => changeShowDetails(newValue)}>Show Details</ShowDetailsButton>
+
+    {#if showDetails}
       <HowItWorks />
     {/if}
-      <Motivation />
-      <SupportOrGiveFeedbackRequest />
-    </div>
-  </main>
-  
-  <!-- <FeedbackForm />
+
+    <p><br /><br /></p>
+    <ShowPhilosophyButton onChange={(newValue) => changeShowPhilosophy(newValue)}>Show Philosophy</ShowPhilosophyButton>
+    {#if showPhilosophy}
+      <Philosophy />
+    {/if}
+  </div>
+</main>
+
+<!-- <FeedbackForm />
   <FeedbackList />
   <FeedbackStats /> -->
-
 <style>
-  </style>
+</style>
--- frontend/cult-playground-challenge-of-the-day/src/components/texts/Philosophy.svelte
+++ frontend/cult-playground-challenge-of-the-day/src/components/texts/Philosophy.svelte
@@ -1,6 +1,25 @@
-Let us play, learn and earn together.
 
-In evolution playing turned out to be the most successful way of learning. 
+<p><br><br></p>
+The games which can be found here might inspire people to inspire their
+friends to explore 
+<a href="https://rumble.com/v1lf51r-cultdao-in-100-seconds-michael-saylor-talks-about-cult.html" target="_blank">
+    CULT 
+</a>
+and 
+<a href="https://rumble.com/v1lf3yb-revolt-2-earn-in-100-seconds-michael-saylor-talks-about-revolt-2-earn.html" target="_blank">
+RVLT
+</a>.
+<br /><br />
+It seems fair to give all people the chance to come on board early. <br><br>
 
-Let us come together in playful creativity to free the world.
+<!-- In evolution playing turned out to be the most successful way of learning. <br><br> -->
 
+Let us play, learn and earn together in playful creativity to free the world. <br /><br />
+
+If you do not like playing such games, you might want to let us know what we can improve by
+<a
+href="https://github.com/cultfamily-on-github/cult-education-games-microservice/issues"
+target="_blank"
+>
+submitting an issue</a
+>.
