8e4ebc5ecaa076151e5dca8da2fa5e9043fbee12
Michael <michael.spengler@sap.com>
2022-09-29 07:40:05 +0200

    improve visitor experience - Article

--- frontend/cult-playground-challenge-of-the-day/src/components/Countdown.svelte
+++ frontend/cult-playground-challenge-of-the-day/src/components/Countdown.svelte
@@ -1,7 +1,7 @@
 <script>
     import { onMount } from "svelte";
 
-    export let deadline
+    export let deadline;
 
     let cD = new Date();
     let utcFullYear = cD.getUTCFullYear();
@@ -16,38 +16,39 @@
 
     let countDownString = "";
 
+    const updateCountdown = () => {
+        const now = new Date().getTime();
+
+        const distance = deadline - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        hours = Math.floor(
+            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    };
     onMount(async () => {
-
-
-        // var countDownDate = new Date("sep 28, 2022 23:59:59").getTime();
-
-        // Update the count down every 1 second
-        var x = setInterval(function () {
-            // Get today's date and time
-            var now = new Date().getTime();
-
-            // Find the distance between now and the count down date
-            var distance = deadline - now;
-
-            // Time calculations for days, hours, minutes and seconds
-            // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            hours = Math.floor(
-                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-            );
-            minutes = Math.floor(
-                (distance % (1000 * 60 * 60)) / (1000 * 60)
-            );
-            seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            // Display the result in the element with id="demo"
-            //   document.getElementById("demo").innerHTML =
-
-            // If the count down is finished, write some text
-            if (distance < 0) {
-                clearInterval(x);
-            }
+        updateCountdown()
+        setInterval(function () {
+            updateCountdown();
         }, 1000);
     });
 </script>
 
-Ends In: {hours}h {minutes}m {seconds}s
+<!-- Ends In: {hours}h {minutes}m {seconds}s -->
+<div class="countdown">
+    <h4>
+        You have {hours}h {minutes}m {seconds}s to complete this game.
+    </h4> 
+</div>
+
+<style>
+    .countdown {
+        /* background: #ff6a95;
+        color: #fff; */
+        font-size: large;
+    }
+</style>
